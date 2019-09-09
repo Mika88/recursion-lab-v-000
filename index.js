5110ea1b9318404519570206a997dab70e06d8c1
Mika Spivak <mikaspivak@gmail.com>
2019-09-09 22:44:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ function includesNumber(array, number) {
     return true
   } else {
     let lastItem = array.pop()
-    includesNumber(array, number) && lastItem === number
+    return includesNumber(array, number) && lastItem === number
   }
 }
